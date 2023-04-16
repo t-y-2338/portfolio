4f4d50822279bbea5db7f0421faefd33c446848c
t-y-2338 <yoshi_rr@MacBook-Air1.local>
2023-04-16 15:10:27 +0900

    ホストを追加

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,8 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.hosts << "portfolio-app-f1kq.onrender.com"
+  
   # Code is not reloaded between requests.
   config.cache_classes = true
 
